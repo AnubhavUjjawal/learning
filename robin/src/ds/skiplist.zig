8827637bc5e8378b80a497b2d94a8e5fc2d69c3d
Anubhav Ujjawal <anubhavujjawal@gmail.com>
2026-02-11 23:47:36 +0530

    feature/value-in-skiplist (#13)
    Reviewed-on: http://rpi1.local:3000/anubhavujjawal/learning/pulls/13
    Co-authored-by: Anubhav Ujjawal <anubhavujjawal@gmail.com>
    Co-committed-by: Anubhav Ujjawal <anubhavujjawal@gmail.com>

--- robin/src/ds/skiplist.zig
+++ robin/src/ds/skiplist.zig
@@ -4,17 +4,22 @@ const mem = std.mem;
 const math = std.math;
 const thread = std.Thread;
 const testing = std.testing;
+const assert = std.debug.assert;
 
 const logger = log.scoped(.skiplist);
 
 /// A thread safe skip list implementation
+///
 /// TODO:
-/// - We can potentially make it lock free
+/// - We can potentially make it lock free. But before going lockfree, add a benchmark setup
+/// - Make sure we support single item pointers as keys (copied during insert), and not only slices
+/// - Add get and delete
+/// - Add prefetch when we are iterating through a node
 pub fn SkipList(
-    comptime T: type,
+    comptime K: type,
+    comptime V: type,
     comptime max_levels: u8,
-    // should include pointer tie breaking in cases of equality
-    comptime cmp: fn (a: *const T, b: *const T) math.Order,
+    comptime cmp: fn (a: *const K, b: *const K) math.Order,
 ) type {
 
     // A Node in SkipList. Has the following characteristics:
@@ -22,96 +27,136 @@ pub fn SkipList(
     // - If the data is a slice, it copies it using @memcpy and keeps it in memory. Does not support
     //   deep copy yet.
     // Data layout
-    // [node] + [padding if needed] + [node.data child if node.data is slice] + [padding] + [node next pointers]
+    // [node] + [padding if needed] + [node.key child if node.key is slice] + [padding] + [node next pointers] + [padding] + [node.value if node.value is slice]
     const Node = struct {
         const Self = @This();
 
         const self_alignment_needed = @alignOf(Self);
         const self_ptrs_alignment_needed = @alignOf(*Self);
+        const key_is_slice = @typeInfo(K) == .pointer and @typeInfo(K).pointer.size == .slice;
+        const value_is_slice = @typeInfo(V) == .pointer and @typeInfo(V).pointer.size == .slice;
 
-        data: ?T,
+        key: ?K,
+        value: ?V,
         levels: u8,
 
+        fn _get_nexts_size(levels: u8) usize {
+            return mem.alignForward(usize, levels * @sizeOf(*Self), _get_value_alignment_needed());
+        }
+
         fn nexts(self: *Self) []?*Self {
-            const ptr_addr = @intFromPtr(self) + Self._get_header_size(self.data) + Self._get_data_size(self.data);
-            const ptr: [*]align(self_ptrs_alignment_needed) ?*Self =
-                @ptrFromInt(ptr_addr);
+            const ptr_addr = @intFromPtr(self) +
+                _get_header_size(self.key) +
+                _get_key_size(self.key);
+            const ptr: [*]align(self_ptrs_alignment_needed) ?*Self = @ptrFromInt(ptr_addr);
             const aligned: [*]?*Self = @ptrCast(@alignCast(ptr));
             return aligned[0..self.levels];
         }
 
+        /// levels is 0 indexed.
         fn setNext(self: *Self, level: u8, next: ?*Self) void {
             self.nexts()[level] = next;
         }
 
+        /// levels is 0 indexed.
         fn getNext(self: *Self, level: u8) ?*Self {
             return self.nexts()[level];
         }
 
+        /// levels is 0 indexed.
         fn insertNext(self: *Self, level: u8, next: *Self) void {
             const current_next = self.getNext(level);
             self.setNext(level, next);
             next.setNext(level, current_next);
         }
 
-        inline fn _get_header_size(data: ?T) usize {
+        inline fn _get_header_size(key: ?K) usize {
             // ideally, we don't need to copy data, unless it is a slice.
             // We could have just taken ownership of passed slices and called it a day, but that doesn't
             // improve cache locality
-            if (data != null and (comptime @typeInfo(T) == .pointer and @typeInfo(T).pointer.size == .slice)) {
-                const self_data_alignment_needed = @alignOf(@typeInfo(T).pointer.child);
+            const self_value_alignment_needed = _get_value_alignment_needed();
+
+            if (key != null and key_is_slice) {
+                const self_key_alignment_needed = _get_key_alignment_needed();
                 // since we store child the data is pointing to as well, we need to update header alignment.
-                return mem.alignForward(usize, @sizeOf(Self), @max(self_data_alignment_needed, self_ptrs_alignment_needed));
+                return mem.alignForward(usize, @sizeOf(Self), @max(self_key_alignment_needed, self_ptrs_alignment_needed, self_value_alignment_needed));
             }
-            return mem.alignForward(usize, @sizeOf(Self), self_ptrs_alignment_needed);
+            return mem.alignForward(usize, @sizeOf(Self), @max(self_ptrs_alignment_needed, self_value_alignment_needed));
         }
 
-        inline fn _get_data_size(data: ?T) usize {
-            if (data != null and (comptime @typeInfo(T) == .pointer and @typeInfo(T).pointer.size == .slice)) {
+        inline fn _get_key_size(data: ?K) usize {
+            if (data != null and key_is_slice) {
                 // since we store our pointers after data, we need to add padding for alignment purposes.
-                return mem.alignForward(usize, data.?.len * @sizeOf(@typeInfo(T).pointer.child), self_ptrs_alignment_needed);
+                return mem.alignForward(usize, data.?.len * @sizeOf(@typeInfo(K).pointer.child), self_ptrs_alignment_needed);
             }
             return 0;
         }
 
-        inline fn _get_node_size(levels: u8, data: ?T) usize {
-            var data_size: usize = 0;
-            if (comptime @typeInfo(T) == .pointer and @typeInfo(T).pointer.size == .slice) {
+        inline fn _get_value_size(data: ?V) usize {
+            if (data != null and value_is_slice) {
+                return data.?.len * @sizeOf(@typeInfo(V).pointer.child);
+            }
+            return 0;
+        }
+
+        inline fn _get_node_size(levels: u8, key: ?K, value: ?V) usize {
+            var key_size: usize = 0;
+            if (key_is_slice) {
                 // since we store our pointers after data, we need to add padding for alignment purposes.
-                data_size = Self._get_data_size(data);
+                key_size = _get_key_size(key);
+            }
+            var value_size: usize = 0;
+            if (value_is_slice) {
+                // since we store our pointers after data, we need to add padding for alignment purposes.
+                value_size = _get_value_size(value);
             }
 
-            const node_size = Self._get_header_size(data) + data_size + (@sizeOf(*Self) * levels);
+            const node_size = _get_header_size(key) + key_size + value_size + _get_nexts_size(levels);
             return node_size;
         }
 
-        inline fn _get_total_alignment_needed() comptime_int {
-            const data_alignment_needed = comptime blk: {
-                const info = @typeInfo(T);
-                if (info == .pointer and info.pointer.size == .slice) break :blk @alignOf(info.pointer.child);
-                break :blk 1;
-            };
-            return @max(self_alignment_needed, self_ptrs_alignment_needed, data_alignment_needed);
+        fn _get_key_alignment_needed() comptime_int {
+            const info = @typeInfo(K);
+            if (key_is_slice) return @alignOf(info.pointer.child);
+            return 1;
         }
 
-        fn create(allocator: mem.Allocator, data: ?T, levels: u8) !*Self {
+        fn _get_value_alignment_needed() comptime_int {
+            const info = @typeInfo(V);
+            if (value_is_slice) return @alignOf(info.pointer.child);
+            return 1;
+        }
+
+        inline fn _get_total_alignment_needed() comptime_int {
+            const key_alignment_needed = _get_key_alignment_needed();
+            const value_alignment_needed = _get_value_alignment_needed();
+            return @max(self_alignment_needed, self_ptrs_alignment_needed, key_alignment_needed, value_alignment_needed);
+        }
+
+        fn create(allocator: mem.Allocator, key: ?K, value: ?V, levels: u8) !*Self {
             if (levels == 0) return error.ZERO_LEVELS_NOT_ALLOWED;
-            const node_size = _get_node_size(levels, data);
-            logger.debug("node size in bytes: {d}, data: {any}", .{ node_size, data });
+            const node_size = _get_node_size(levels, key, value);
+            logger.debug("node size in bytes: {d}, data: {any}", .{ node_size, key });
             const bytes = try allocator
-                .alignedAlloc(u8, comptime mem.Alignment.fromByteUnits(Self._get_total_alignment_needed()), node_size);
+                .alignedAlloc(u8, comptime mem.Alignment.fromByteUnits(_get_total_alignment_needed()), node_size);
             const node: *Self = @ptrCast(@alignCast(bytes.ptr));
 
-            node.* = .{ .data = data, .levels = levels };
+            node.* = .{ .key = key, .levels = levels, .value = value };
 
-            const header_size = Self._get_header_size(data);
+            const header_size = _get_header_size(key);
+            if (key != null and key_is_slice) {
+                const child_data: [*]@typeInfo(K).pointer.child = @ptrFromInt(@intFromPtr(bytes.ptr) + header_size);
+                const dest = child_data[0..key.?.len];
+                @memcpy(dest, key.?);
+                node.key = dest;
+            }
 
-            if (data != null and (comptime @typeInfo(T) == .pointer and @typeInfo(T).pointer.size == .slice)) {
-                // since we store child the data is pointing to as well, we need to update header alignment.
-                const child_data: [*]@typeInfo(T).pointer.child = @ptrFromInt(@intFromPtr(bytes.ptr) + header_size);
-                const dest = child_data[0..data.?.len];
-                @memcpy(dest, data.?);
-                node.data = dest;
+            if (value != null and value_is_slice) {
+                const body_size = header_size + _get_key_size(key) + _get_nexts_size(levels);
+                const child_data: [*]@typeInfo(V).pointer.child = @ptrFromInt(@intFromPtr(bytes.ptr) + body_size);
+                const dest = child_data[0..value.?.len];
+                @memcpy(dest, value.?);
+                node.value = dest;
             }
 
             // Initialize the trailing pointers to null
@@ -122,8 +167,8 @@ pub fn SkipList(
         }
 
         fn destroy(allocator: mem.Allocator, node: *Self) void {
-            const node_size = _get_node_size(node.levels, node.data);
-            const bytes_ptr: [*]align(Self._get_total_alignment_needed()) u8 = @ptrCast(@alignCast(node));
+            const node_size = _get_node_size(node.levels, node.key, node.value);
+            const bytes_ptr: [*]align(_get_total_alignment_needed()) u8 = @ptrCast(@alignCast(node));
             const original_allocation = bytes_ptr[0..node_size];
             allocator.free(original_allocation);
         }
@@ -142,12 +187,15 @@ pub fn SkipList(
         /// TODO(verify): I think using a fixed buffer allocator here would increase cache hit rate.
         pub fn init(allocator: mem.Allocator, prng: std.Random) !Self {
             logger.debug("initialising the skiplist", .{});
-            const head = try Node.create(allocator, null, max_levels);
+            const head = try Node.create(allocator, null, null, max_levels);
             errdefer Node.destroy(allocator, head);
             return .{ .allocator = allocator, .head = head, .lock = .{}, .prng = prng };
         }
 
-        pub fn len(self: *const Self) usize {
+        // consider making len return an atomic load
+        pub fn len(self: *Self) usize {
+            self.lock.lockShared();
+            defer self.lock.unlockShared();
             return self._len;
         }
 
@@ -163,8 +211,8 @@ pub fn SkipList(
         }
 
         /// insert element into the skip list.
-        pub fn insert(self: *Self, element: T) !void {
-            logger.debug("adding item: {any}", .{element});
+        pub fn insert(self: *Self, key: K, value: V) !void {
+            logger.debug("adding item: {any}", .{key});
             self.lock.lock();
             defer self.lock.unlock();
 
@@ -181,7 +229,7 @@ pub fn SkipList(
             var curr_level = max_levels;
             while (curr_level > 0) {
                 const next = curr_node.getNext(curr_level - 1);
-                const cmp_result: ?math.Order = if (next != null) compare(&next.?.data.?, &element) else null;
+                const cmp_result: ?math.Order = if (next != null) compare(&next.?.key.?, &key) else null;
                 if (next != null and (cmp_result == .lt or cmp_result == .eq)) {
                     curr_node = next.?;
                 } else {
@@ -190,6 +238,7 @@ pub fn SkipList(
                     stack[@as(usize, @intCast(stack_idx))] = curr_node;
                 }
             }
+            assert(stack_idx == max_levels - 1);
 
             // insertion logic:
             // while elements in stack
@@ -202,7 +251,7 @@ pub fn SkipList(
                 if (!should_insert) break;
                 levels += 1;
             }
-            const node = try Node.create(self.allocator, element, levels);
+            const node = try Node.create(self.allocator, key, value, levels);
             errdefer Node.destroy(self.allocator, node);
 
             for (0..levels) |level| {
@@ -214,7 +263,7 @@ pub fn SkipList(
             self._len += 1;
         }
 
-        /// A very basic debug print
+        /// A very basic debug print, not thread safe
         /// We need to improve it
         pub fn debugPrint(self: *const Self) void {
             var curr: ?*Node = self.head;
@@ -222,7 +271,7 @@ pub fn SkipList(
             while (current_level > 0) {
                 curr = self.head;
                 while (curr != null) {
-                    std.debug.print("{any} -> ", .{curr.?.data});
+                    std.debug.print("{any} -> ", .{curr.?.key});
                     curr = curr.?.getNext(current_level - 1);
                 }
                 std.debug.print("\n", .{});
@@ -234,11 +283,6 @@ pub fn SkipList(
 }
 
 pub fn bytesCompare(a: *const []const u8, b: *const []const u8) math.Order {
-    // switch (mem.order(u8, a.*, b.*)) {
-    //     .gt => return false,
-    //     .eq => return @intFromPtr(a.ptr) < @intFromPtr(b.ptr),
-    //     .lt => return true,
-    // }
     return mem.order(u8, a.*, b.*);
 }
 
@@ -250,22 +294,24 @@ test "sanity test insert int" {
     const allocator = testing.allocator;
     var prng = std.Random.DefaultPrng.init(@as(u64, testing.random_seed));
     var random = prng.random();
-    var l = try SkipList(u32, 12, u32Compare).init(allocator, random);
+    var l = try SkipList(u32, u32, 12, u32Compare).init(allocator, random);
     defer l.deinit();
 
     const num_inserts = 10_000;
     for (0..num_inserts) |_| {
         const num = random.int(u32);
-        try l.insert(num);
+        try l.insert(num, num % 100);
     }
 
     var curr: ?@TypeOf(l.head) = l.head;
     const current_level = 0;
     var total_count: u32 = 0;
     var prev: ?@TypeOf(l.head) = null;
+    try testing.expect(l.len() == num_inserts);
     while (curr != null) {
         if (prev != null and prev != l.head) {
-            try testing.expect(prev.?.data.? <= curr.?.data.?);
+            try testing.expect(prev.?.key.? <= curr.?.key.?);
+            try testing.expect(curr.?.key.? % 100 == curr.?.value.?);
         }
         prev = curr;
         curr = curr.?.getNext(current_level);
@@ -281,22 +327,27 @@ test "sanity test insert string" {
     const allocator = testing.allocator;
     var prng = std.Random.DefaultPrng.init(@as(u64, testing.random_seed));
     var random = prng.random();
-    var l = try SkipList([]const u8, 12, bytesCompare).init(allocator, random);
+    var l = try SkipList([]const u8, []const u8, 12, bytesCompare).init(allocator, random);
     defer l.deinit();
 
-    var str: [100]u8 = undefined;
+    var key: [100]u8 = undefined;
     const num_inserts = 10_000;
     for (0..num_inserts) |_| {
-        random.bytes(&str);
-        try l.insert(&str);
+        random.bytes(&key);
+        // being lazy and inserting the same in both, sorries, bad test
+        try l.insert(&key, &key);
     }
     var curr: ?@TypeOf(l.head) = l.head;
     const current_level = 0;
     var total_count: u32 = 0;
     var prev: ?@TypeOf(l.head) = null;
+    var c: usize = 0;
+    try testing.expect(l.len() == num_inserts);
     while (curr != null) {
+        c += 1;
         if (prev != null and prev != l.head) {
-            try testing.expect(bytesCompare(&prev.?.data.?, &curr.?.data.?) == .lt or bytesCompare(&prev.?.data.?, &curr.?.data.?) == .eq);
+            try testing.expect(bytesCompare(&prev.?.key.?, &curr.?.key.?) == .lt or bytesCompare(&prev.?.key.?, &curr.?.key.?) == .eq);
+            try testing.expect(bytesCompare(&curr.?.key.?, &curr.?.value.?) == .eq);
         }
         prev = curr;
         curr = curr.?.getNext(current_level);
